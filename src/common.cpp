297920b4fa8f284f51d6897aa7e636302556ebce
mazx <dyxdy@live.com>
2022-09-07 16:04:18 +0800

    test: update test for fp32.

--- src/common.cpp
+++ src/common.cpp
@@ -1,13 +1,16 @@
 #include "common.h"
+#include "cuda_utils.h"
 
 namespace ape {
 
 cublasHandle_t ape_cublas_handle = nullptr;
+cudaDeviceProp ape_gpu_prop;
 
 void apeInit() {
     if (ape_cublas_handle == nullptr) {
-        cublasCreate(&ape_cublas_handle);
-        cublasSetMathMode(ape_cublas_handle, CUBLAS_DEFAULT_MATH);
+        cublasSafeCall(cublasCreate(&ape_cublas_handle));
+        cublasSafeCall(cublasSetMathMode(ape_cublas_handle, CUBLAS_DEFAULT_MATH));
+        cudaSafeCall(cudaGetDeviceProperties(&ape_gpu_prop, 0));
     }
 }
 
---
+++ test/test_gemm_fp32_fp32b.cpp
@@ -0,0 +1,13 @@
+#include "ape.h"
+#include "test/test_gemm_fp32.h"
+
+int main() {
+    ape::apeInit();
+    ape::test::test_gemm_fp32(128, 128, 128, ape::APE_ALGO_FP32B);
+    ape::test::test_gemm_fp32(256, 256, 256, ape::APE_ALGO_FP32B);
+    ape::test::test_gemm_fp32(512, 512, 512, ape::APE_ALGO_FP32B);
+    ape::test::test_gemm_fp32(1024, 1024, 1024, ape::APE_ALGO_FP32B);
+    ape::test::test_gemm_fp32(2048, 2048, 2048, ape::APE_ALGO_FP32B);
+    ape::test::test_gemm_fp32(4096, 4096, 4096, ape::APE_ALGO_FP32B);
+    ape::test::test_gemm_fp32(8192, 8192, 8192, ape::APE_ALGO_FP32B);
+}
